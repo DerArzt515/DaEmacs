e944648db15387c69809621dbfcff14f2ed9532c
Matt Cavalier <matthewjcavalier@pm.me>
2021-09-27 14:53:46 -0500

    added init

---
+++ init.el
@@ -0,0 +1,17 @@
+(load "~/.daEmacs/main.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" default))
+ '(package-selected-packages
+   '(org-roam evil-magit counsel-projectile vterm all-the-icons-ivy-rich general helpful ivy-rich ivy-rick rainbow-delimiters doom-themes doom-modeline counsel ivy evil use-package spacegray-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
